aeeeb4103d7faa894e04279e32387191745d5069
Lyubov <144713098+lyubov78@users.noreply.github.com>
2023-11-05 18:40:43 +0300

    task_1.py

--- Лабораторная работа 4/task_1.py
+++ Лабораторная работа 4/task_1.py
@@ -3,11 +3,11 @@ import random
 
 def get_unique_list_numbers() -> list[int]:
     '''Создает список случайных чисел в диапозоне от -10 до 10'''
-    random_list = []
+    random_list = set()
     for num in range(15):
         num = random.randint(-10, 10)
-        random_list.append(num)
-    return random_list
+        random_list.add(num)
+    return list(random_list)
 
 
 list_unique_numbers = get_unique_list_numbers()
