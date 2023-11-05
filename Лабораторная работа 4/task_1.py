dce3bbae5f2262977b829c24caaf24a289cb90ab
Lyubov <144713098+lyubov78@users.noreply.github.com>
2023-11-05 19:06:47 +0400

    Add files via upload

---
+++ Лабораторная работа 4/task_1.py
@@ -0,0 +1,15 @@
+import random
+
+
+def get_unique_list_numbers() -> list[int]:
+    '''Создает список случайных чисел в диапозоне от -10 до 10'''
+    random_list = []
+    for num in range(15):
+        num = random.randint(-10, 10)
+        random_list.append(num)
+    return random_list
+
+
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
