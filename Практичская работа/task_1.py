60c52f769c2de735ed79baf905508b5bd80026a2
Lyubov <144713098+lyubov78@users.noreply.github.com>
2023-11-14 18:10:58 +0400

    Add files via upload

---
+++ Практичская работа/task_1.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+def get_unique_list_numbers() -> list[int]:
+    '''Создает список случайных чисел в диапозоне от -10 до 10'''
+    random_nums = set()
+    while len(random_nums) < 15:
+        random_nums.add(randint(-10, 10))
+    return list(random_nums)
+
+
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
